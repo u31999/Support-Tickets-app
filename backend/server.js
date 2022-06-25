98e82546d6cfdc7709d6d204a65753b319a15f0d
u31999 <u31999@gmail.com>
2022-06-25 08:10:37 -0700

    Add routes-contollers-middelware folders and files

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log('...Hello')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 8000
+const {errorHanller} = require('../backend/middleware/errorMidlleware')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+    res.status(200).res.json({message: 'You are welcome'})
+})
+
+
+// Routes
+app.use('/api/users', require('./routes/userRoute'))
+app.use(errorHanller)
+
+app.listen(PORT, 
+    () => console.log(`Server running in port ${PORT}`))

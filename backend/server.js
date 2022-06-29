0355dea826790ac26ca48721fe82d42866b72526
u31999 <u31999@gmail.com>
2022-06-29 03:47:01 -0700

    Add heroku script

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
 const coonectDB = require('./config/db')
@@ -12,14 +13,21 @@ coonectDB()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req, res) => {
-    res.status(200).res.json({message: 'You are welcome'})
-})
-
-
 // Routes
 app.use('/api/users', require('./routes/userRoute'))
 app.use('/api/tickets', require('./routes/tiketRoute'))
+
+// Serve front-end
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+} else {
+    app.get('/', (req, res) => {
+    res.status(200).res.json({message: 'You are welcome'})
+    })
+}
+
 app.use(errorHanller)
 
 app.listen(PORT, 
